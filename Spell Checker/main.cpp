f9a7602153c1efea55f0560a633767d9f4d9953b
Clara Fairbanks <clarafairbanks@claras-macbook-pro.local>
2018-11-14 10:14:23 -0500

    Initial Commit

---
+++ Spell Checker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Spell Checker
+//
+//  Created by Clara Fairbanks on 11/14/18.
+//  Copyright © 2018 Clara Fairbanks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
